f5553f7741c79c25cc8d0e0079d9e8b53dd57f71
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-03-30 21:56:17 +0100

    Hello

---
+++ lib/CPAN/Test/Dummy/HasScript.pm
@@ -0,0 +1,23 @@
+package CPANPLUS::Test::Dummy::HasScript;
+
+#ABSTRACT: CPANPLUS Test Dummy
+
+use strict;
+use warnings;
+
+q[Test me oh test me, test me if you think you've heard this one before];
+
+=head1 SYNOPSIS
+
+ # erm
+
+ cpanp -i ACME::CPANPLUS::Module::With::Core::PreReq
+
+=head1 DESCRIPTION
+
+CPANPLUS::Test::Dummy::HasScript is a fake module that has a script file
+so I can test something in L<CPANPLUS> and L<CPANPLUS::YACSmoke>
+
+No moving parts and nothing to see.
+
+=cut
